5b5d1d70da97bfefcd995a46adf098ce29e1e3a5
Yann Voté <ygversil@lilo.org>
2019-10-29 10:24:51 +0100

    Update translations

--- i18n/fr.ts
+++ i18n/fr.ts
@@ -3,9 +3,75 @@
 <context>
     <name>@default</name>
     <message>
-        <location filename="test_translations.py" line="48"/>
+        <location filename="../test/test_translations.py" line="48"/>
         <source>Good morning</source>
         <translation>Bonjour</translation>
+    </message>
+</context>
+<context>
+    <name>GuidedOfflineEditingPlugin</name>
+    <message>
+        <location filename="../guided_offline_editing.py" line="187"/>
+        <source>&amp;Guided Offline Editing Plugin</source>
+        <translation>Extension édition hors-ligne assistée</translation>
+    </message>
+    <message>
+        <location filename="../guided_offline_editing.py" line="175"/>
+        <source>Guided Offline Editing</source>
+        <translation>Édition hors-ligne assistée</translation>
+    </message>
+    <message>
+        <location filename="../guided_offline_editing.py" line="212"/>
+        <source>No project file</source>
+        <translation>Aucun fichier projet</translation>
+    </message>
+    <message>
+        <location filename="../guided_offline_editing.py" line="212"/>
+        <source>Please save the project to a file first.</source>
+        <translation>Merci d'enregistrer le projet dans un projet.</translation>
+    </message>
+</context>
+<context>
+    <name>GuidedOfflineEditingPluginDialogBase</name>
+    <message>
+        <location filename="../guided_offline_editing_dialog_base.ui" line="14"/>
+        <source>Guided Offline Editing Plugin</source>
+        <translation>Extension édition hors-ligne assistée</translation>
+    </message>
+    <message>
+        <location filename="../guided_offline_editing_dialog_base.ui" line="27"/>
+        <source>TextLabel</source>
+        <translation>TextLabel</translation>
+    </message>
+    <message>
+        <location filename="../guided_offline_editing_dialog_base.ui" line="125"/>
+        <source>Download</source>
+        <translation>Télécharger</translation>
+    </message>
+    <message>
+        <location filename="../guided_offline_editing_dialog_base.ui" line="54"/>
+        <source>Select themes to download</source>
+        <translation>Sélectionner les thème à télécharger</translation>
+    </message>
+    <message>
+        <location filename="../guided_offline_editing_dialog_base.ui" line="75"/>
+        <source>Downloaded layers</source>
+        <translation>Télécharger les couches</translation>
+    </message>
+    <message>
+        <location filename="../guided_offline_editing_dialog_base.ui" line="104"/>
+        <source>Relations between layers</source>
+        <translation>Relations entre couches</translation>
+    </message>
+    <message>
+        <location filename="../guided_offline_editing_dialog_base.ui" line="163"/>
+        <source>Upload</source>
+        <translation>Téléverser</translation>
+    </message>
+    <message>
+        <location filename="../guided_offline_editing_dialog_base.ui" line="143"/>
+        <source>Offline layers</source>
+        <translation>Couches hors-lignes</translation>
     </message>
 </context>
 </TS>
